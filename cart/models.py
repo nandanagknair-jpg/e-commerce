9a2bc9b8799f44622325165335e8205a89edff10
nandanagknair-jpg <nandanagknair@gmail.com>
2026-09-24 11:47:34 +0530

    Update models.py
    to cart

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,9 @@
+from django.conf import settings
 from django.db import models
-
-# Create your models here.
+from products.models import Product
+class Cart(models.Model):
+user=usermodels.OneToOneField(settings .AUTH_USER_MODEL, on_delete=models. CASCADE, related_name='cart')
+class CartItem(models.Model):
+ cart = models.ForeignKey(Cart, on_delete=models.CASCADE,related_name='items')
+ product = models.ForeignKey(Product, on_delete=models.CASCADE)quantity = models.PositiveIntegerField(default=1)
+  classMeta:classconstraints[models.UniqueConstraint(fields=['cart','product'], name='unique_cart_product')]
